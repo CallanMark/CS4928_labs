fe395e5674dabdfc7d1fa3cf7a50c0f4555281c1
Mark <markcallan101@gmail.com>
2025-11-30 19:03:16 +0000

    Add all code provided in part B

--- src/main/java/com/cafepos/app/ReceiptFormatter.java
+++ src/main/java/com/cafepos/app/ReceiptFormatter.java
@@ -7,8 +7,7 @@ public String format(long id, List<LineItem> items,com.cafepos.pricing.PricingSe
 StringBuilder sb = new StringBuilder();
 sb.append("Order #").append(id).append("\n");
 for (LineItem li : items) {
-sb.append(" - ").append(li.product().name()).append("x").append(li.quantity())
-.append(" = ").append(li.lineTotal()).append("\n");
+sb.append(" - ").append(li.product().name()).append("x").append(li.quantity()).append(" = ").append(li.lineTotal()).append("\n");
 }
 
 sb.append("Subtotal: ").append(pr.subtotal()).append("\n");
@@ -16,8 +15,7 @@ if (pr.discount().asBigDecimal().signum() > 0) {
 sb.append("Discount: -").append(pr.discount()).append("\n");
 }
 
-sb.append("Tax (").append(taxPercent).append("%):
-").append(pr.tax()).append("\n");
+sb.append("Tax (").append(taxPercent).append("%): ").append(pr.tax()).append("\n");
 sb.append("Total: ").append(pr.total());
 return sb.toString();
 }
---
+++ src/main/java/com/cafepos/ui/ConsoleView.java
@@ -0,0 +1,6 @@
+
+package com.cafepos.ui;
+
+public final class ConsoleView {
+public void print(String s) { System.out.println(s); }
+}
